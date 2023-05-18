f920c261d22d52a6c21813758d60587323c9bb4c
ojitos369 <ojitos369@gmail.com>
2023-05-18 17:32:01 -0600

    cambiando theme in front

--- front/src/Components/MyComponent/index.jsx
+++ front/src/Components/MyComponent/index.jsx
@@ -1,30 +1,25 @@
-import React from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 import { AllContext } from '../../App/MyContext';
 import { ModalThemeChanged } from '../Modals/ModalThemeChanged';
 
 function MyComponent() {
-    const { ls, lf, s, f, Icons } = React.useContext(AllContext);
+    const { ls, lf, s, f, Icons } = useContext(AllContext);
     const icons = new Icons();
     return (
-        <React.Fragment>
+        <Fragment>
             <div className='flex flex-wrap justify-center'>
                 <h2 className={`text-center basis-full mt-3 font-bold text-3xl ${ls?.theme === 'black' ? 'text-white' : 'text-black'}`}
                 >
                     Actual theme: {ls.theme}
-                    <br />
-                    {s?.init?.saludo || ":'c"}
                 </h2>
-                <button
-                    className="btn btn-primary basis-1/4 mt-3"
-                    onClick={lf.toggleTheme}
-                    >
-                    Change Theme
-                    <span className='text-icon'>{icons.brush()}</span>
-                </button>
-                <h3 className='text-center w-full mt-3'>{s?.main?.message}</h3>
+                <h4 className={`text-center basis-full mt-3 font-bold text-xl ${ls?.theme === 'black' ? 'text-white' : 'text-black'}`}
+                >
+                    {s?.init?.saludo || ":'c"}
+                </h4>
             </div>
+            {/* {s.modals?.themes?.changed && <ModalThemeChanged zindex={-1} />} */}
             {s.modals?.themes?.changed && <ModalThemeChanged />}
-        </React.Fragment>
+        </Fragment>
     )
 }
 
