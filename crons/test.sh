b53e51db7badea755681354bacad671fcb4f4bfb
ojitos369 <ojitos369@gmail.com>
2025-01-21 14:39:45 -0600

    [CHANGE] load env crons metohd

--- crons/test.sh
+++ crons/test.sh
@@ -1,2 +1,4 @@
-export $(grep -v '^#' /usr/src/app/.env | xargs)
+# export $(grep -v '^#' /usr/src/app/.env | xargs)
+#! /bin/bash
+. /usr/src/app/.env
 /usr/local/bin/python /usr/src/app/manage.py testing
