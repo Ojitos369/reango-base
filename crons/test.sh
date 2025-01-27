5fcaba8ace3cf3cbb7451f98aa3fc6420e2fb6ef
Erick Garcia <ojitos369@gmail.com>
2025-01-27 17:47:35 -0600

    Update test.sh

--- crons/test.sh
+++ crons/test.sh
@@ -1,4 +1,6 @@
-# export $(grep -v '^#' /usr/src/app/.env | xargs)
-#! /bin/bash
-. /usr/src/app/.env
+while IFS= read -r line; do
+    if [[ "$line" != \#* && "$line" =~ "=" ]]; then
+        export "$line"
+    fi
+done < /usr/src/app/.env
 /usr/local/bin/python /usr/src/app/manage.py testing
