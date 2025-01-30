f3b7e55ce760bb356ceab434305b2fa2c18ba61e
Erick Garcia <ojitos369@gmail.com>
2025-01-30 13:48:26 -0600

    Update test.sh

--- crons/test.sh
+++ crons/test.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+# export $(grep -v '^#' /usr/src/app/.env | xargs)
 while IFS= read -r line; do
-    if [[ "$line" != \#* && "$line" =~ "=" ]]; then
-        export "$line"
+    echo "in line $line"
+    if [[ "$line" != \#* && "$line" == *=* ]]; then
+        key=$(echo "$line" | cut -d= -f1)
+        value=$(echo "$line" | cut -d= -f2- | sed 's/^"//;s/"$//')
+        echo "exporting $key=\"$value\""
+        export "$key=$value"
     fi
 done < /usr/src/app/.env
 /usr/local/bin/python /usr/src/app/manage.py testing
